3e266f5a77e8c66180f45bd0b64503e316008305
zxl <xiaolongzong@drwine.cn>
2018-06-28 18:18:15 +0800

    独立oauth服务

--- src/main/java/com/landasoft/eureka/LandaMasEurekaApplication.java
+++ src/main/java/com/landasoft/eureka/LandaMasEurekaApplication.java
@@ -1,10 +1,10 @@
-package com.landasoft.mas;
+package com.landasoft.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.landasoft")
 @EnableEurekaServer
 public class LandaMasEurekaApplication {
 
